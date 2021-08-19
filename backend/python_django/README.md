09473e1974ce02ef31d30fdf037de23fbe63c711
Ahmad <zoaby.am@gmail.com>
2021-08-20 02:03:33 +0300

    chore: update media & .env config

--- backend/python_django/README.md
+++ backend/python_django/README.md
@@ -4,8 +4,7 @@
 
 ## Getting started
 
-1. Create a `.env` file in the root directory `python_django` and fill the needed environment variables, check [.env.example](./.env.example) for reference.
-2. Run `docker compose up` which will spin up 3 docker containers:
+Run `docker compose up` which will spin up 3 docker containers:
   
      1. Django application [http://0.0.0.0:8000](http://0.0.0.0:8000)
      2. PostgreSQL database (Available only through docker network (tweety))
--- backend/python_django/config/urls.py
+++ backend/python_django/config/urls.py
@@ -1,6 +1,8 @@
 """Main application urlconf."""
 
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
@@ -13,5 +15,14 @@ admin.site.index_title = _("Tweety Administration")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    media_urlpatterns: list = static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    dev_urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+        *media_urlpatterns,
+    ]
+    urlpatterns += dev_urlpatterns
