03a43cb8b395a6aa82ffaa85c09715ee6c74a37e
Ahmad <zoaby.am@gmail.com>
2021-11-26 00:22:30 +0200

    feat: add support for oauth2 flows

--- backend/python_django/apps/users/views.py
+++ backend/python_django/apps/users/views.py
@@ -1,7 +1,8 @@
 """Users views."""
 
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from oauth2_provider.contrib.rest_framework.permissions import TokenHasResourceScope, TokenHasScope
+from rest_framework import permissions, viewsets
 
 from .serializers import UserSerializer
 
@@ -11,6 +12,10 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     """API view set for the User model."""
 
+    permission_classes = [
+        permissions.IsAdminUser | TokenHasResourceScope | TokenHasScope
+    ]
     queryset = User.objects.exclude(is_staff=True).all()
     serializer_class = UserSerializer
     lookup_field = "username"
+    required_scopes = ["users"]
--- backend/python_django/config/urls.py
+++ backend/python_django/config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
+from oauth2_provider.urls import base_urlpatterns, oidc_urlpatterns
 
 admin.site.site_title = _("Tweety Admin Site")
 
@@ -14,10 +15,18 @@ admin.site.site_header = _("Tweety Administration")
 admin.site.index_title = _("Tweety Administration")
 
 
+class Oauth2Urls:
+    """Oauth2 Url config."""
+
+    urlpatterns = base_urlpatterns + oidc_urlpatterns
+    app_name = "oauth2_provider"
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.api.v1_urls", namespace="api")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("oauth2/", include(Oauth2Urls, namespace="oauth2_provider")),
 ]
 
 media_patterns: list = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
